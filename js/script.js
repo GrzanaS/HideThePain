c138764199774bfa71548da08e11868db8801bba
Grzegorz Szudzik <grzegorzszudzik93@gmail.com>
2023-04-12 19:23:30 +0200

    Created new script

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let button = document.querySelector(".body__button");
+let article = document.querySelector(".body__article");
+let header = document.querySelector(".body__header__1");
+let articleOne = document.querySelector(".body__article__1");
+let headerOne = document.querySelector(".body__header__2");
+let articleTwo = document.querySelector(".body__article__2");
+let articleThree = document.querySelector(".body__article__3");
+
+button.addEventListener("click", () => {
+    article.innerText = "More like András István Arató. He's a very famous and main character of many memes with people who are trying hide their pain by forced smile."
+    header.innerText = "Who is real Harold?"
+    articleOne.innerText = "András is a Hungarian enqineer living in Budapest. He's married and have son. After his retire, some photographer saw him and proposed to him photo session that presented many casual professions like carpenter or lawyer"
+    headerOne.innerText = "Transformation"
+    articleTwo.innerText = "After few months, András discovered in web many funny pictures with his face."
+    articleThree.innerText = "From the beginning, András didn't like it at all. He had a little hope that users will forget about him and his Harold. But it didn't happen that way. After all he accepted his role and started to used this for his own advantage."
+});
