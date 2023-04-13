9605e60f655ab7487f499829981a876ca9e1ae27
Grzegorz Szudzik <grzegorzszudzik93@gmail.com>
2023-04-13 20:25:21 +0200

    Finishing the script

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ let articleOne = document.querySelector(".body__article__1");
 let headerOne = document.querySelector(".body__header__2");
 let articleTwo = document.querySelector(".body__article__2");
 let articleThree = document.querySelector(".body__article__3");
+let articleFour = document.querySelector(".body__article__4");
+let articleFive = document.querySelector(".body__article__5");
 
 button.addEventListener("click", () => {
     article.innerText = "More like András István Arató. He's a very famous and main character of many memes with people who are trying hide their pain by forced smile."
@@ -13,4 +15,6 @@ button.addEventListener("click", () => {
     headerOne.innerText = "Transformation"
     articleTwo.innerText = "After few months, András discovered in web many funny pictures with his face."
     articleThree.innerText = "From the beginning, András didn't like it at all. He had a little hope that users will forget about him and his Harold. But it didn't happen that way. After all he accepted his role and started to used this for his own advantage."
+    articleFour.innerText = "He started speaking on the events and festivals. Thanks to his fame, he worked as DJ in local broadcasting station for five years. Also in 2019 he became a face of Coca-cola on Hungary."
+    articleFive.innerText = "He also performed in music video by Cloud 9+, to track called Hide the Pain"
 });
